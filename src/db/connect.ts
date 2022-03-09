0d9cb6cbb635ede9f3fd6b2aa0193db13421f76e
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-03-09 14:49:59 +0700

    connection pool option

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = (url: string) => {
-    return mongoose.connect(url);
-  }
-  
- export default connectDB;
+  return mongoose.connect(url, { maxPoolSize: 15 });
+};
+
+export default connectDB;
