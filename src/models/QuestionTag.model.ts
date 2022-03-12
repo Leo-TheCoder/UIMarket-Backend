0c2411fc4679e015f972447d767143ad7f1b5645
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-03-12 23:08:02 +0700

    constrain length of tagName

--- src/models/QuestionTag.model.ts
+++ src/models/QuestionTag.model.ts
@@ -4,6 +4,8 @@ const QuestionTagSchema = new mongoose.Schema({
     tagName: {
         type: String,
         required:[true, "Please provide tag name"],
+        maxlength: 20,
+        minlength: 2,
     },
     totalQuestion: {
         type: Number,
