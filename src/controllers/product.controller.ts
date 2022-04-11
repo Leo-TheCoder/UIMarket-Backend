4bb8cbb0ba78da3da11f027425ca19e2091e0606
Ngô Nhật Tân <ngonhattan22@gmail.com>
2022-04-11 20:01:53 +0700

    find by Id and name

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -57,4 +57,93 @@ const findByCategory = async (req: Request, res: Response) => {
   });
 };
 
-export { findByCategory };
+const findById = async (req: Request, res: Response) => {
+  const product = await ProductModel.find({
+    _id: req.params.productId,
+    productStatus: 1,
+  }).lean();
+
+  if (!product) {
+    throw new NotFoundError("Invalid product Id");
+  } else {
+    res.status(StatusCodes.OK).json({ product });
+  }
+};
+
+const findByName = async (req: Request, res: Response) => {
+  const query = req.query as IQuery;
+  const page = parseInt(query.page!) || Constants.defaultPageNumber;
+  const limit = parseInt(query.limit!) || Constants.defaultLimit;
+
+  const totalProduct = await ProductModel.aggregate([
+    {
+      $search: {
+        index: "productName",
+        text: {
+          path: "productName",
+          query: req.params.productName,
+        },
+      },
+    },
+    { $match: { productStatus: 1 } },
+    { $count: "total" },
+  ]);
+  const total = totalProduct[0].total;
+
+  const totalPages =
+    total % limit === 0
+      ? Math.floor(total / limit)
+      : Math.floor(total / limit) + 1;
+
+  const product = await ProductModel.aggregate([
+    {
+      $search: {
+        index: "productName",
+        text: {
+          path: "productName",
+          query: req.params.productName,
+        },
+      },
+    },
+    { $match: { productStatus: 1 } },
+    {
+      $addFields: {
+        score: { $meta: "searchScore" },
+      },
+    },
+    { $skip: (page - 1) * limit },
+    { $limit: limit },
+    {
+      $lookup: {
+        from: "categories",
+        localField: "productCategory",
+        foreignField: "_id",
+        pipeline: [{ $project: { categoryName: 1 } }],
+        as: "productCategory",
+      },
+    },
+    {
+      $lookup: {
+        from: "shops",
+        localField: "shopId",
+        foreignField: "_id",
+        pipeline: [{ $project: { shopName: 1 } }],
+        as: "shop",
+      },
+    },
+  ]);
+
+  res.status(StatusCodes.OK).json({
+    totalPages,
+    page,
+    limit,
+    product,
+  });
+};
+
+export {
+  findByCategory,
+  findById,
+  findByName,
+  //
+};
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { compulsoryAuth, optionalAuth } from "../middlewares/authentication";
-import { findByCategory } from "../controllers/product.controller";
+import {
+  findByCategory,
+  findById,
+  findByName,
+} from "../controllers/product.controller";
 
 const router = express.Router();
 
 //GET Method
 router.get("/category/:categoryId", findByCategory);
+router.get("/:productId", findById);
+router.get("/search/:productName", findByName);
 
 //POST Method
 //PUT Method
