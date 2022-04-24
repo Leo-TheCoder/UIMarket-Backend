9de7409913c9da3731806cc9d4ea9edad1409474
Ngô Nhật Tân <ngonhattan22@gmail.com>
2022-04-24 22:20:33 +0700

    Squashed commit of the following:
    commit 373a4fecc2b1396bd9da00efa864e57801553f17
    Author: Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
    Date:   Wed Apr 20 23:35:01 2022 +0700
    
        Test connect with Paypal

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -4,6 +4,8 @@ import {
   captureOrder,
   cancelPayment,
   payoutOrder,
+  returnAfterLoginPaypal,
+  authorizationEndpoint,
 } from "../controllers/payment.controller";
 
 const router = express.Router();
@@ -12,5 +14,7 @@ router.post("/create-order", createOrder);
 router.get("/capture-order", captureOrder);
 router.get("/cancel-payment", cancelPayment);
 router.post("/payout", payoutOrder);
+router.get("/after-login", returnAfterLoginPaypal);
+router.get("/authorization-endpoint", authorizationEndpoint)
 
 export default router;
