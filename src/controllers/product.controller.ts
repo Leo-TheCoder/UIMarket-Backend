618745230e84f01f9674f4b168d07a7269fcdacb
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-05-05 22:44:57 +0700

    get products from all shop, sort, filter

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -7,15 +7,127 @@ import { NotFoundError } from "../errors";
 import ShopModel from "../models/Shop.model";
 import * as ErrorMessage from "../errors/error_message";
 
+enum SortTypes {
+  MoneyAsc = "money-asc",
+  MoneyDes = "money-des",
+  NameAsc = "name-asc",
+  NameDes = "name-des",
+}
+
+enum FilterTypes {
+  PRICE_FREE = "money-free",
+  PRICE_0_19 = "money-0-19",
+  PRICE_20_39 = "money-20-39",
+  PRICE_40_59 = "money-40-59",
+  PRICE_60_79 = "money-60-79",
+  PRICE_80 = "money-80",
+}
 interface IQuery {
   page?: string;
   limit?: string;
+  sort?: SortTypes;
+  filter?: FilterTypes;
 }
+
+const sortObjMongoose = (sort?: SortTypes): any => {
+  if (sort === SortTypes.MoneyAsc) {
+    return { productPrice: 1 };
+  }
+  if (sort === SortTypes.MoneyDes) {
+    return { productPrice: -1 };
+  }
+  if (sort === SortTypes.NameAsc) {
+    return { productName: 1 };
+  }
+  if (sort === SortTypes.NameDes) {
+    return { productName: -1 };
+  }
+  return {};
+};
+
+const filterObjMongoose = (filter?: FilterTypes) => {
+  switch (filter) {
+    case FilterTypes.PRICE_FREE:
+      return { productPrice: 0 };
+    case FilterTypes.PRICE_0_19:
+      return { productPrice: { $gt: 0, $lte: 19 } };
+    case FilterTypes.PRICE_20_39:
+      return { productPrice: { $gte: 20, $lte: 39 } };
+    case FilterTypes.PRICE_40_59:
+      return { productPrice: { $gte: 40, $lte: 59 } };
+    case FilterTypes.PRICE_60_79:
+      return { productPrice: { $gte: 60, $lte: 79 } };
+    case FilterTypes.PRICE_80:
+      return { productPrice: { $gte: 80 } };
+    default:
+      return {};
+  }
+};
+
+const projectionProductList = {
+  __v: 0,
+  productDescription: 0,
+};
+
+export const getAllProducts = async (req: Request, res: Response) => {
+  const query = req.query as IQuery;
+  const page = parseInt(query.page!) || Constants.defaultPageNumber;
+  const limit = parseInt(query.limit!) || Constants.defaultLimit;
+
+  const sort = query.sort;
+  const sortObj = sortObjMongoose(sort);
+
+  const filter = query.filter;
+  const filterObj = filterObjMongoose(filter);
+
+  const total = await ProductModel.countDocuments({
+    productStatus: 1,
+    ...filterObj,
+  }).lean();
+
+  const totalPages =
+    total % limit === 0
+      ? Math.floor(total / limit)
+      : Math.floor(total / limit) + 1;
+
+  //Get product
+  const products = await ProductModel.find(
+    {
+      productStatus: 1,
+      ...filterObj,
+    },
+    projectionProductList
+  )
+    .sort(sortObj)
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .populate({ path: "productCategory", select: ["categoryName"] })
+    .lean();
+
+  const productsResult = products.map((product) => {
+    //get first picture
+    product.productPicture = product.productPicture[0];
+    return product;
+  });
+
+  return res.status(StatusCodes.OK).json({
+    totalPages,
+    page,
+    limit,
+    products: productsResult,
+  });
+};
 
 const findByCategory = async (req: Request, res: Response) => {
   const query = req.query as IQuery;
   const page = parseInt(query.page!) || Constants.defaultPageNumber;
   const limit = parseInt(query.limit!) || Constants.defaultLimit;
+
+  const sort = query.sort;
+  const sortObj = sortObjMongoose(sort);
+
+  const filter = query.filter;
+  const filterObj = filterObjMongoose(filter);
 
   //Checking valid category
   const category = await CategoryModel.findById(req.params.categoryId).lean();
@@ -27,6 +139,7 @@ const findByCategory = async (req: Request, res: Response) => {
   const total = await ProductModel.countDocuments({
     productCategory: req.params.categoryId,
     productStatus: 1,
+    ...filterObj,
   }).lean();
   const totalPages =
     total % limit === 0
@@ -39,7 +152,9 @@ const findByCategory = async (req: Request, res: Response) => {
     .find({
       productCategory: req.params.categoryId,
       productStatus: 1,
+      ...filterObj,
     })
+    .sort(sortObj)
     .skip((page - 1) * limit)
     .limit(limit)
     .populate({ path: "productCategory", select: ["categoryName"] })
@@ -71,6 +186,12 @@ const findByName = async (req: Request, res: Response) => {
   const page = parseInt(query.page!) || Constants.defaultPageNumber;
   const limit = parseInt(query.limit!) || Constants.defaultLimit;
 
+  const sort = query.sort;
+  const sortObj = sortObjMongoose(sort);
+
+  const filter = query.filter;
+  const filterObj = filterObjMongoose(filter);
+
   const totalProduct = await ProductModel.aggregate([
     {
       $search: {
@@ -81,7 +202,7 @@ const findByName = async (req: Request, res: Response) => {
         },
       },
     },
-    { $match: { productStatus: 1 } },
+    { $match: { productStatus: 1 , ...filterObj} },
     { $count: "total" },
   ]);
   const total = totalProduct[0].total;
@@ -101,10 +222,11 @@ const findByName = async (req: Request, res: Response) => {
         },
       },
     },
-    { $match: { productStatus: 1 } },
+    { $match: { productStatus: 1, ...filterObj } },
     { $addFields: { score: { $meta: "searchScore" } } },
     { $skip: (page - 1) * limit },
     { $limit: limit },
+    { $sort: sortObj},
     {
       $lookup: {
         from: "categories",
@@ -139,6 +261,12 @@ const getProductsByShop = async (req: Request, res: Response) => {
   const page = parseInt(query.page!) || Constants.defaultPageNumber;
   const limit = parseInt(query.limit!) || Constants.defaultLimit;
 
+  const sort = query.sort;
+  const sortObj = sortObjMongoose(sort);
+
+  const filter = query.filter;
+  const filterObj = filterObjMongoose(filter);
+
   //Check shop ID
   const shop = await ShopModel.find({ _id: shopId, shopStatus: 1 }).lean();
   if (!shop) {
@@ -149,6 +277,7 @@ const getProductsByShop = async (req: Request, res: Response) => {
   const total = await ProductModel.countDocuments({
     shopId: shopId,
     productStatus: 1,
+    ...filterObj,
   }).lean();
 
   const totalPages =
@@ -157,7 +286,12 @@ const getProductsByShop = async (req: Request, res: Response) => {
       : Math.floor(total / limit) + 1;
 
   //Get product
-  const products = await ProductModel.find({ shopId: shopId, productStatus: 1 })
+  const products = await ProductModel.find({
+    shopId: shopId,
+    productStatus: 1,
+    ...filterObj,
+  })
+    .sort(sortObj)
     .skip((page - 1) * limit)
     .limit(limit)
     .populate({ path: "productCategory", select: ["categoryName"] })
