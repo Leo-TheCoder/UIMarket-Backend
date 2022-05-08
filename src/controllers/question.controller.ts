4d4937beaad1f2bce338e9f9340817ff6b7fd8ce
Ngô Nhật Tân <ngonhattan22@gmail.com>
2022-05-08 16:13:51 +0700

    resolve bounty question automatically

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -1,4 +1,18 @@
+//Library
 import { StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
+import { IUserRequest } from "../types/express";
+import * as Constants from "../constants";
+import { getStatusVote } from "../utils/statusVote";
+import { pointRollBack, pointTransaction } from "../utils/currencyTransaction";
+
+//Model
+import Question from "../models/Question.model";
+import QuestionTagModel from "../models/QuestionTag.model";
+import AnswerModel from "../models/Answer.model";
+
+//Error
+import * as ErrorMessage from "../errors/error_message";
 import {
   BadRequestError,
   ForbiddenError,
@@ -6,15 +20,7 @@ import {
   InternalServerError,
   NotFoundError,
 } from "../errors";
-import { Request, Response } from "express";
-import { IUserRequest } from "../types/express";
-import Question from "../models/Question.model";
-import QuestionTagModel from "../models/QuestionTag.model";
-import * as Constants from "../constants";
-import AnswerModel from "../models/Answer.model";
-import { getStatusVote } from "../utils/statusVote";
-import { pointRollBack, pointTransaction } from "../utils/currencyTransaction";
-import * as ErrorMessage from "../errors/error_message";
+
 interface IQuery {
   page?: string;
   limit?: string;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "express-async-errors";
 import express, { Request, Response, Application } from "express";
 import cors from "cors";
 import connectDB from "./db/connect";
+import cron from "node-cron";
 
 //Create server app instance
 const app: Application = express();
@@ -63,11 +64,20 @@ app.use("/api/v1/licenses", licenseRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+//Schedule Task
+import { resolveBounty } from "./scheduled/resolveBounty";
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI!);
     app.listen(PORT, (): void => {
       console.log(`Server is listening on port ${PORT}...`);
+    });
+
+    //Scheduled Tasks
+    //Resolve bounty run everyday at 00:01
+    cron.schedule("0 1 * * *", async () => {
+      await resolveBounty();
     });
   } catch (error) {
     console.log(error);
