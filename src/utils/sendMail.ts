2e2c77d350c8be0c7a2005146ab3ca85cf995f9e
KoahF <khoakg2013@gmail.com>
2022-05-10 10:48:01 +0700

    Change email verify link

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,64 +1,64 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  from: "deex.uimarket@gmail.com",
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
+	service: 'Gmail',
+	from: 'deex.uimarket@gmail.com',
+	auth: {
+		user: process.env.EMAIL_USERNAME,
+		pass: process.env.EMAIL_PASSWORD,
+	},
 });
 
 const sendVerifyEmail = (to: string, userId: string, verifyCode: string) => {
-  const url = `${process.env.DOMAIN_NAME}/api/v1/verify?userId=${userId}&verifyCode=${verifyCode}`;
-  transporter.sendMail(
-    {
-      from: "<no-reply> deex.uimarket@gmail.com",
-      to: to,
-      subject: `Verify Account`,
-      html: `Click <a href = '${url}'>here</a> to confirm your email.`,
-    },
-    (err) => {
-      if (err) console.log(err?.message);
-    }
-  );
+	const url = `${process.env.FE_DOMAIN_NAME}/verify?userId=${userId}&verifyCode=${verifyCode}`;
+	transporter.sendMail(
+		{
+			from: '<no-reply> deex.uimarket@gmail.com',
+			to: to,
+			subject: `Verify Account`,
+			html: `Click <a href = '${url}'>here</a> to confirm your email.`,
+		},
+		(err) => {
+			if (err) console.log(err?.message);
+		}
+	);
 };
 
 const sendForgetPasswordEmail = (
-  to: string,
-  userId: string,
-  verifyCode: string
+	to: string,
+	userId: string,
+	verifyCode: string
 ) => {
-  const url = `${process.env.FE_DOMAIN_NAME}/resetforgotpassword?userId=${userId}&verifyCode=${verifyCode}`;
-  transporter.sendMail(
-    {
-      from: "<no-reply> deex.uimarket@gmail.com",
-      to: to,
-      subject: `Forget Password`,
-      html: `Click <a href = '${url}'>here</a> to reset your password`,
-    },
-    (err) => {
-      if (err) console.log(err.message);
-    }
-  );
+	const url = `${process.env.FE_DOMAIN_NAME}/resetforgotpassword?userId=${userId}&verifyCode=${verifyCode}`;
+	transporter.sendMail(
+		{
+			from: '<no-reply> deex.uimarket@gmail.com',
+			to: to,
+			subject: `Forget Password`,
+			html: `Click <a href = '${url}'>here</a> to reset your password`,
+		},
+		(err) => {
+			if (err) console.log(err.message);
+		}
+	);
 };
 
 const sendResetPasswordConfirmEmail = (to: string) => {
-  transporter.sendMail(
-    {
-      from: "<no-reply> deex.uimarket@gmail.com",
-      to: to,
-      subject: `Your Password has been reset`,
-      html: `Just want to say that your password has been changed!`,
-    },
-    (err) => {
-      if (err) console.log(err.message);
-    }
-  );
+	transporter.sendMail(
+		{
+			from: '<no-reply> deex.uimarket@gmail.com',
+			to: to,
+			subject: `Your Password has been reset`,
+			html: `Just want to say that your password has been changed!`,
+		},
+		(err) => {
+			if (err) console.log(err.message);
+		}
+	);
 };
 
 export {
-  sendVerifyEmail,
-  sendForgetPasswordEmail,
-  sendResetPasswordConfirmEmail,
+	sendVerifyEmail,
+	sendForgetPasswordEmail,
+	sendResetPasswordConfirmEmail,
 };
