08b5fe3d12002ea92cefd0b9e1be48c85a76450a
Ngô Nhật Tân <ngonhattan22@gmail.com>
2022-05-22 14:48:08 +0700

    get user reviews

--- src/routes/review.route.ts
+++ src/routes/review.route.ts
@@ -4,6 +4,7 @@ import {
   createReview,
   getProductReviews,
   getReviewById,
+  getUserReview,
   updateReview,
 } from "../controllers/review.controller";
 
@@ -12,6 +13,7 @@ const router = express.Router();
 //GET Method
 router.get("/:productId", getProductReviews);
 router.get("/detail/:reviewId", getReviewById);
+router.get("/user/list", compulsoryAuth, getUserReview);
 
 //POST Method/
 router.post("/:invoiceId/:productId", compulsoryAuth, createReview);
