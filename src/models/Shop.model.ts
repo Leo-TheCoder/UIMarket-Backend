bc1306725692d967956f9ab99e239d8c08f37266
Duc-Lam-jh <duclam227@gmail.com>
2022-05-27 20:49:59 +0700

    fix: shopPaypal -> ShopPayPal

--- src/models/Shop.model.ts
+++ src/models/Shop.model.ts
@@ -1,55 +1,55 @@
-import mongoose from "mongoose";
-import { defaultMinLength } from "../constants";
+import mongoose from 'mongoose';
+import { defaultMinLength } from '../constants';
 
 const ShopSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Types.ObjectId,
-      ref: "Customer",
-      required: [true, "Please provide user id"],
-      immutable: true,
-    },
-    shopName: {
-      type: String,
-      required: [true, "Please provide shop name"],
-      minlength: defaultMinLength / 2,
-    },
-    shopPaypal: {
-      type: String,
-    },
-    shopDescription: {
-      type: String,
-      required: [true, "Please provide shop description"],
-      minlength: defaultMinLength,
-    },
-    shopPhone: {
-      type: String,
-      required: false,
-      length: 10,
-    },
-    shopEmail: {
-      type: String,
-      required: [true, "Please provide shop email"],
-    },
-    shopBanner: {
-      type: String,
-      required: false,
-    },
-    shopStatus: {
-      type: Number,
-      default: 1,
-      enum: [0, 1],
-    },
-    shopBalance: {
-      type: Number,
-      default: 0,
-    },
-    taxCode: {
-      type: String,
-      default: null,
-    },
-  },
-  { timestamps: true },
+	{
+		userId: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Customer',
+			required: [ true, 'Please provide user id' ],
+			immutable: true,
+		},
+		shopName: {
+			type: String,
+			required: [ true, 'Please provide shop name' ],
+			minlength: defaultMinLength / 2,
+		},
+		shopPayPal: {
+			type: String,
+		},
+		shopDescription: {
+			type: String,
+			required: [ true, 'Please provide shop description' ],
+			minlength: defaultMinLength,
+		},
+		shopPhone: {
+			type: String,
+			required: false,
+			length: 10,
+		},
+		shopEmail: {
+			type: String,
+			required: [ true, 'Please provide shop email' ],
+		},
+		shopBanner: {
+			type: String,
+			required: false,
+		},
+		shopStatus: {
+			type: Number,
+			default: 1,
+			enum: [ 0, 1 ],
+		},
+		shopBalance: {
+			type: Number,
+			default: 0,
+		},
+		taxCode: {
+			type: String,
+			default: null,
+		},
+	},
+	{ timestamps: true },
 );
 
-export default mongoose.model("Shop", ShopSchema);
+export default mongoose.model('Shop', ShopSchema);
