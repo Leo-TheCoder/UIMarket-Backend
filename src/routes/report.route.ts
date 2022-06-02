55f304e65bc43db89eb7703f1b17a4bf37c1918a
Ngô Nhật Tân <ngonhattan22@gmail.com>
2022-06-02 21:28:43 +0700

    fix report route

--- src/routes/report.route.ts
+++ src/routes/report.route.ts
@@ -1,16 +1,10 @@
 import express from "express";
 import { compulsoryAuth, optionalAuth } from "../middlewares/authentication";
-import {
-  createReport,
-  reportListEC,
-  reportListEdu,
-} from "../controllers/report.controller";
+import { createReport } from "../controllers/report.controller";
 
 const router = express.Router();
 
 //GET Method
-router.get("/reportList/Edu", reportListEdu);
-router.get("/reportList/EC", reportListEC);
 
 //POST Method
 router.post("/", createReport);
