26e08c6ef4eff0541e150a0b36e2125a07fb074c
Võ Minh Lâm <64176716+Leo-TheCoder@users.noreply.github.com>
2022-06-04 10:54:11 +0700

    update total question

--- src/controllers/questionTag.controller.ts
+++ src/controllers/questionTag.controller.ts
@@ -3,23 +3,28 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 
 interface IQuery {
-    tagName?: string
+  tagName?: string;
 }
 
 const getTags = async (req: Request, res: Response) => {
-    const query = req.query as IQuery;
-    const tagName = query.tagName || null;
+  const query = req.query as IQuery;
+  const tagName = query.tagName || null;
 
-    if(!tagName || tagName?.length < 1) {
-        //return res.status(StatusCodes.NO_CONTENT).send();
-        const tags = await QuestionTagModel.find().select("_id tagName").lean();
-        return res.status(StatusCodes.OK).json(tags);
-    }
+  if (!tagName || tagName?.length < 1) {
+    //return res.status(StatusCodes.NO_CONTENT).send();
+    const tags = await QuestionTagModel.find()
+      .select("_id tagName totalQuestion")
+      .lean();
+    return res.status(StatusCodes.OK).json(tags);
+  }
 
-    const regexp = new RegExp("^" + tagName);
-    const tagsDoc = await QuestionTagModel.find({tagName: regexp}, {'_id': 1, 'tagName': 1}).lean();
+  const regexp = new RegExp("^" + tagName);
+  const tagsDoc = await QuestionTagModel.find(
+    { tagName: regexp },
+    { _id: 1, tagName: 1, totalQuestion: 1 }
+  ).lean();
 
-    res.status(StatusCodes.OK).json(tagsDoc);
-}
+  res.status(StatusCodes.OK).json(tagsDoc);
+};
 
-export {getTags}
+export { getTags };
